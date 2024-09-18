72922849f711352a8979f24b1f8e18070240041e
OFF <peerapat13th@gmail.com>
2024-09-19 03:19:06 +0700

    new components

---
+++ src/app/components/mobile-detail/mobile-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile-detail.component.html',
+  styleUrl: './mobile-detail.component.css'
+})
+export class MobileDetailComponent {
+
+}
