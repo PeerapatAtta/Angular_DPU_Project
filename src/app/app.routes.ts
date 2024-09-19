246806f6c1aa95e042e63c7829a3be1e71a9d38b
OFF <peerapat13th@gmail.com>
2024-09-20 00:57:48 +0700

    new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { CartComponent } from './components/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+    },
+    {
+        path: 'home',
+        title: 'Home',
+        component: HomeComponent,
+    },    
+    {
+        path: 'product',
+        title: 'Product',
+        component: ProductComponent,
+    },
+    {
+        path: 'cart',
+        title: 'Cart',
+        component: CartComponent,
+    },
+    {
+        path: 'stock',
+
+    },
+    {
+        path: 'event',
+
+    },
+    {
+        path: '**',
+        redirectTo: 'login',
+    }
+];
